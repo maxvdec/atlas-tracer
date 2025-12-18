ba02b76c53e6c46415ba63f4c12af4f1db2ace79
Max Van den Eynde <maxvdec@gmail.com>
2025-12-18 07:31:49 +0100

    Finished the resource debugging view

--- Atlas Tracer/Client/Interpreter.swift
+++ Atlas Tracer/Client/Interpreter.swift
@@ -9,6 +9,25 @@ import Combine
 import Foundation
 import SwiftUI
 
+enum DebugDrawCallType: Int, Codable {
+    case draw = 1
+    case indexed = 2
+    case patched = 3
+}
+
+enum DebugResourceType: Int, Codable {
+    case texture = 1
+    case buffer = 2
+    case shader = 3
+    case mesh = 4
+}
+
+enum DebugResourceOperation: Int, Codable {
+    case created = 1
+    case loaded = 2
+    case unloaded = 3
+}
+
 protocol Interpreter {
     func incoming(_ message: String)
 }
@@ -19,12 +38,6 @@ struct DebugLog: Codable, Equatable {
     let file: String
     let line: Int
     let type: String
-}
-
-enum DebugDrawCallType: Int, Codable {
-    case draw = 1
-    case indexed = 2
-    case patched = 3
 }
 
 struct DrawCallInfo: Codable, Equatable {
@@ -57,12 +70,50 @@ struct FrameDrawCallInfo: Codable, Equatable {
     }
 }
 
+struct DebugResourceEvent: Codable, Equatable {
+    let type: String
+    let callerObject: String
+    let resourceType: DebugResourceType
+    let operation: DebugResourceOperation
+    let frameNumber: Int
+    let sizeMb: Float
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case callerObject = "caller_object"
+        case resourceType = "resource_type"
+        case frameNumber = "frame_number"
+        case sizeMb = "size_mb"
+        case operation
+    }
+}
+
+struct DebugFrameResourceInformation: Codable, Equatable {
+    let type: String
+    let frameNumber: Int
+    let resourcesCreated: Int
+    let resourcesUnloaded: Int
+    let resourcesLoaded: Int
+    let totalMemoryMb: Float
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case frameNumber = "frame_number"
+        case resourcesCreated = "resources_created"
+        case resourcesUnloaded = "resources_unloaded"
+        case resourcesLoaded = "resources_loaded"
+        case totalMemoryMb = "total_memory_mb"
+    }
+}
+
 final class DebugInformation: ObservableObject {
     static let shared = DebugInformation()
 
     @Published var logs: [DebugLog] = []
     @Published var drawCalls: [DrawCallInfo] = []
     @Published var frameDrawInsights: [FrameDrawCallInfo] = []
+    @Published var resourceEvents: [DebugResourceEvent] = []
+    @Published var frameResourceInformation: [DebugFrameResourceInformation] = []
 
     func addLog(_ log: DebugLog) {
         DispatchQueue.main.async {
@@ -106,6 +157,18 @@ class MainInterpreter: Interpreter {
             if let info = try? JSONDecoder().decode(FrameDrawCallInfo.self, from: data) {
                 DispatchQueue.main.async {
                     DebugInformation.shared.frameDrawInsights.append(info)
+                }
+            }
+        case "resource_event":
+            if let info = try? JSONDecoder().decode(DebugResourceEvent.self, from: data) {
+                DispatchQueue.main.async {
+                    DebugInformation.shared.resourceEvents.append(info)
+                }
+            }
+        case "frame_resources_info":
+            if let info = try? JSONDecoder().decode(DebugFrameResourceInformation.self, from: data) {
+                DispatchQueue.main.async {
+                    DebugInformation.shared.frameResourceInformation.append(info)
                 }
             }
         default:
